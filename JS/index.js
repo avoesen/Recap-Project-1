ae1ad49a7fe861806fa62c7e4cf5931914b51d7d
Anna von Oesen <annavonoesen@gmail.com>
2024-01-18 16:59:54 +0100

    add js function

--- JS/index.js
+++ JS/index.js
@@ -1,28 +1,31 @@
 
 
- const answerBtn = document.querySelector("#answer")
- const answerText = document.querySelector("#answer-text") 
- const bookmarkEl = document.querySelector("#bookmark") 
+ const answerBtns = document.querySelectorAll(".answer")
+ const answerTexts = document.querySelectorAll(".answer-text") 
 
- const questionCard = document.querySelector(".question-card")
- 
- console.log(questionCard)
+const questionCards = document.querySelectorAll(".question-card");
+const questionCard = document.querySelector(".question-card");
 
- answerBtn.addEventListener('click', () => {
-    if (answerBtn.textContent === "Show Answer"){
-    answerBtn.textContent = "Hide Answer";
-    answerText.style.display = 'block';}
-    else {answerBtn.textContent = "Show Answer";
-    answerText.style.display = 'none';
-    }
-})
+answerBtns.forEach(function(answerBtn) {
+    answerBtn.addEventListener("click", () => {
+      if (answerBtn.textContent === "Show Answer") {
+        answerBtn.textContent = "Hide Answer";
+        answerBtn.nextElementSibling.style.display = "block";
+      } else {
+        answerBtn.textContent = "Show Answer";
+        answerBtn.nextElementSibling.style.display = "none";
+      }
+    });
+});
 
+const bookmarkEls = document.querySelectorAll(".bookmark"); 
+const bookmarkEl = document.querySelector(".bookmark");
 
 //bookmark in anderes html speichern??
-bookmarkEl.addEventListener('click', () => {
-    bookmarkEl.style.fill = "black";
-    //mit local storage? geht auch mit addclass??
-    localStorage.setItem("questionCardSave", questionCard.outerHTML) 
-})
-
-
+bookmarkEls.forEach((bookmark) => {
+  bookmark.addEventListener("click", () => {
+    bookmark.style.fill = "black";
+    //mit local storage? geht auch mit addclass?? wird überschrieben
+    localStorage.setItem("questionCardSave", questionCard.outerHTML);
+  });
+});
--- JS/profile.js
+++ JS/profile.js
@@ -2,8 +2,6 @@ const bodyElement = document.querySelector('[data-js="body"]');
 
 const darkSwitch = document.querySelector('.switch input')
 
-console.log(darkSwitch)
-
 darkSwitch.addEventListener('click', () => {
         bodyElement.classList.toggle("dark")
   });  
