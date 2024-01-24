976752f695b73a1bc6efab910a73965e046f8508
Anna von Oesen <155978924+avoesen@users.noreply.github.com>
2024-01-24 17:05:04 +0100

    change create Card

--- JS/index.js
+++ JS/index.js
@@ -29,3 +29,37 @@ bookmarkEls.forEach((bookmark) => {
     localStorage.setItem("questionCardSave", questionCard.outerHTML);
   });
 });
+
+const formQuestion = document.querySelector("[data-js='form']");
+const submitButton = document.querySelector("[data-js='submit-button']");
+
+formQuestion.addEventListener("submit", (event) =>{
+  event.preventDefault();
+  const questionForm = new FormData(event.target);
+  const questionData = Object.fromEntries(questionForm); 
+  // return questionData;
+
+  const newQuestion = document.createElement("section");
+  newQuestion.classList.add("question-card");
+  // p tag für Frage und Antwort nicht im InnerHtml ->sicherheitsrisko, da code mit übergeben werden kann
+  newQuestion.innerHTML = `
+            <a class="bookhead">
+                <?xml version="1.0" encoding="utf-8"?>
+                <svg class="bookmark" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M6 6C6 4.89543 6.89543 4 8 4H16C17.1046 4 18 4.89543 18 6V21L12 15L6 21V6Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            </a>    
+            <p>${questionData.question}</p>
+            <button class="answer">Show Answer</button>
+                <p class="answer-text">${questionData.answer}</p>               
+            <div class="tags">
+                <a class="tag">#css</a>
+                <a class="tag">#html</a>
+                <a class="tag">#flexbox</a>
+            </div>
+      `;
+      // keine ahnung wie ich das einem anderen html zuordnen kann 
+  const bodyIndexHTML = document.querySelector("[data-js='body-index']");
+  bodyIndexHTML.append(newQuestion); 
+})
+
